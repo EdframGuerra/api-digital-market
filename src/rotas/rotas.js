15aa6e937d1e5c1a61762973bf9e32d592cc8746
Edfram-Guerra <edfram.guerra@gmail.com>
2023-08-07 11:20:46 -0300

    Implementado: Controlador de usuario, variaveis de ambiente e conexão com banco de dados

---
+++ src/rotas/rotas.js
@@ -0,0 +1,9 @@
+const exppress = require('express');
+const { cadastrarUsuario } = require('../controladores/controladorUsuario');
+
+const rotas = express.Router()
+
+rotas.post('/usuario', cadastrarUsuario)
+
+
+module.exports = rotas;
