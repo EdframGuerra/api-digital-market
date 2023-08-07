286f0382dba38a662d612dac82d6714e4a49d009
Edfram-Guerra <edfram.guerra@gmail.com>
2023-08-07 11:00:43 -0300

    Firts Commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+
+const app = require('./servidor')
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+    console.log(`Started ${new Date().toLocaleString('pt-br')}`)
+})
